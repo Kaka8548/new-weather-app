17ded25cc838151ba81f35299756157adaaf3ba1
Kaka8548 <katyakatya8548@gmail.com>
2022-06-17 02:23:42 +0400

    Added fah-cels converter; changed status data

--- src/src.js
+++ src/src.js
@@ -4,6 +4,13 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
 
 axios.get(apiUrl).then(showCurrentWeather);
 
+let f = document.querySelector("#fah-temp");
+let c = document.querySelector("#cels-temp");
+f.addEventListener("click", showFahTemp);
+c.addEventListener("click", showCelsTemp);
+
+let celsiusTemperature = null;
+
 function showCurrentWeather(response) {
   console.log(response.data);
   weekDays = [
@@ -19,8 +26,8 @@ function showCurrentWeather(response) {
   let day = weekDays[date.getDay()];
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  let temperature = response.data.main.temp;
-  let skyStatus = response.data.weather[0].main;
+  celsiusTemperature = response.data.main.temp;
+  let skyStatus = response.data.weather[0].description;
   let humidity = response.data.main.humidity;
   let windSpeed = response.data.wind.speed;
 
@@ -34,11 +41,27 @@ function showCurrentWeather(response) {
   let currentData = document.querySelector("#currently-data");
   currentData.innerHTML = `${day}, ${hours}:${minutes}`;
   let temp = document.querySelector("#temp");
-  temp.innerHTML = Math.round(temperature);
+  temp.innerHTML = Math.round(celsiusTemperature);
   let status = document.querySelector("#status");
   status.innerHTML = skyStatus;
   let hum = document.querySelector("#hum");
   hum.innerHTML = `Humidity: ${humidity}%`;
   let wind = document.querySelector("#wind");
   wind.innerHTML = `Wind: ${windSpeed} km/h`;
+}
+
+function showFahTemp(event) {
+  event.preventDefault();
+  let fahTemp = document.querySelector("#temp");
+  fahTemp.innerHTML = Math.round(celsiusTemperature * 1.8 + 32);
+  f.classList.add("active");
+  c.classList.remove("active");
+}
+
+function showCelsTemp(event) {
+  event.preventDefault();
+  let CelsTemp = document.querySelector("#temp");
+  CelsTemp.innerHTML = Math.round(celsiusTemperature);
+  c.classList.add("active");
+  f.classList.remove("active");
 }
